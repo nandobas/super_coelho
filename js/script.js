2304e21364b5cc0d58ef855e9f2853d4e0c1341c
nandobas <fernando.basilio@gmail.com>
2022-07-23 16:30:38 -0300

    level interval

--- js/script.js
+++ js/script.js
@@ -7,30 +7,61 @@ var gameRun = false;
 class classDataset{
     classLevel = {arbusto:'animate-arbusto-level-1'};
     level = 1;
-    score = 0;
+    startDateTime = new Date('01/01/2022 00:00:01');
+    endDateTime = new Date('01/01/2022 23:59:59');
+    dif = 0;
 }
 var datasetUser = new classDataset()
+class DateTimeDiference{
+    constructor(startDate, endDate) {
+        this.diference = endDate - startDate;
+        var _segundo = 1000;
+        var _minuto = _segundo * 60;
+        var _hora = _minuto * 60;
+        var _dia = _hora * 24;
+
+        this.dias = Math.floor(this.diference / _dia);
+        this.horas = Math.floor((this.diference % _dia) / _hora);
+        this.minutos = Math.floor((this.diference % _hora) / _minuto);
+        this.segundos = Math.floor((this.diference % _minuto) / _segundo);
+        this.milisegundos = Math.floor(this.diference % _segundo );
+    }
+}
+
+function pad(num, size) {
+    num = num.toString();
+    while (num.length < size) num = "0" + num;
+    return num;
+}
 
 const loop = setInterval(()=>{
     const cloudsPosition = arbusto.offsetLeft;
     const arbustoPosition = arbusto.offsetLeft;
     const bunnyBottom = +window.getComputedStyle(bunny).bottom.replace('px', '');
 
-    if(arbustoPosition <= 180 && arbustoPosition >0){
-        if(bunnyBottom < 80){
-            stopAnimation(arbustoPosition, bunnyBottom, cloudsPosition);
-        }
-    }
-    if(gameRun)
-    switch (datasetUser.level) {
-        case 1:     
-            if(arbustoPosition >=0 && arbustoPosition <=20){
-                incrementScore();
+    if(gameRun){
+        if(arbustoPosition <= 180 && arbustoPosition >0){
+            if(bunnyBottom < 80){
+                stopAnimation(arbustoPosition, bunnyBottom, cloudsPosition);
             }
-            break;
+        }    
+        datasetUser.endDateTime = new Date();
+        dif = new DateTimeDiference(datasetUser.startDateTime, datasetUser.endDateTime);
+        dateTimeScore = pad(dif.minutos, 2) + ':' +  pad(dif.segundos, 2) + ':' + dif.milisegundos.toString().substring(2,0);
+        userscore.innerHTML = dateTimeScore;        
     }
 
 }, 10);
+
+const checkLevel = setInterval(()=>{    
+    datasetUser.endDateTime = new Date();
+    diference = new DateTimeDiference(datasetUser.startDateTime, datasetUser.endDateTime);        
+
+    if(diference.segundos==30){
+        incrementLevel();
+    }
+
+}, 1000);
 
 const move = (event) =>{
     const keyName = event.key;
@@ -46,24 +77,17 @@ function bunnyJump() {
     }, 500);
 }
 
-function incrementScore(){
-    datasetUser.score ++;
-    userscore.innerHTML = datasetUser.score; 
-    if(datasetUser.score == 100){
-        incrementLevel()
-    }
-}
-
 function incrementLevel(){
     arbusto.classList.remove( datasetUser.classLevel.arbusto );
+    arbusto.removeAttribute("style");
     datasetUser.level++;
-    datasetUser.classLevel.arbusto = 'animate-arbusto-level-'+datasetUser.level;
+    datasetUser.classLevel.arbusto = `animate-arbusto-level-${datasetUser.level}`;
     arbusto.classList.add( datasetUser.classLevel.arbusto );
 }
 
 function startAnimation(){
     gameRun = true;
-    datasetUser.score = 0;
+    datasetUser.startDateTime = new Date();
     datasetUser.level = 1;
     datasetUser.classLevel.arbusto = `animate-arbusto-level-1`;
 
@@ -76,7 +100,6 @@ function startAnimation(){
 }
 
 function stopAnimation(arbustoPosition, bunnyBottom, cloudsPosition) {
-    console.log(datasetUser);
     gameRun = false;
     arbusto.classList.remove( datasetUser.classLevel.arbusto );
     arbusto.style.left = `${arbustoPosition}px`;
